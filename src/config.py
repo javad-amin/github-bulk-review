51d3ec2a960ee3c4d97032c11c4bdd29b0204510
Javad Amin <62642707+javad-amin@users.noreply.github.com>
2023-06-26 21:17:03 +0200

    Update src/config.py
    Use Self from typing
    
    Co-authored-by: Grigory Glukhov <grigory.glukhov@billogram.com>

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from typing import Self
+
 from configparser import ConfigParser
 
 CONFIG_FILE_NAME = "config.ini"
@@ -7,13 +9,13 @@ DEFAULT_SECTION_NAME = "GitHub"
 class GithubConfig:
     config = ConfigParser()
 
-    def setup_config(self: "GithubConfig") -> None:
+    def setup_config(self: Self) -> None:
         self.config.read(CONFIG_FILE_NAME)
         if DEFAULT_SECTION_NAME not in self.config.sections():
             self.config.add_section(DEFAULT_SECTION_NAME)
 
     def get(
-        self: "GithubConfig",
+        self: Self,
         key: str,
         fallback: str = "",
         section: str = DEFAULT_SECTION_NAME,
@@ -21,7 +23,7 @@ class GithubConfig:
         return self.config.get(section, key, fallback=fallback)
 
     def update(
-        self: "GithubConfig",
+        self: Self,
         key: str,
         value: str,
         section: str = DEFAULT_SECTION_NAME,
